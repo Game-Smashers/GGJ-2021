c5955fd2983892f29cd579f7270027a94a740680
SeleckyErik <35656626+SeleckyErik@users.noreply.github.com>
2021-01-30 13:35:40 +0100

    Hook up HUD power bar with the factory script

--- Scenes/Factory.gd
+++ Scenes/Factory.gd
@@ -5,9 +5,10 @@ extends Node2D
 var held_object: Node2D = null
 var held_object_start = Vector2(0, 0)
 
-onready var progress_bar = $ProgressBar
+onready var hud: HUD = $CanvasLayer/HUD
 
 # Rooms
+onready var pump_room = $Rooms/WaterPumpRoom
 onready var control_room = $Rooms/ControlRoom
 onready var cafeteria = $Rooms/Cafeteria
 onready var waste_room = $Rooms/WasteRoom
@@ -27,36 +28,36 @@ var rooms = []
 func _ready():
 	for node in get_tree().get_nodes_in_group("grabbable"):
 		node.connect("clicked", self, "_on_grabbable_clicked")
-	
+
 	# TODO: Add water pump room
 	rooms = [control_room, cafeteria, waste_room, reactor_room, turbine_room]
-	
+
 	# Get all levels
 	var levels_dir_path = "res://Levels/"
 	var level_names = []
 	var levels_dir = Directory.new()
 	levels_dir.open(levels_dir_path)
 	levels_dir.list_dir_begin()
-	
+
 	while true:
 		var file = levels_dir.get_next()
 		if file == "":
 			break
 		elif not file.begins_with("."):
 			level_names.append(levels_dir_path + file)
-	
+
 	levels_dir.list_dir_end()
-	
+
 	for level_name in level_names:
 		levels.append(load(level_name))
-	
+
 	var current_level = levels[current_level_index]
 	time_left = current_level.TimeLimitSeconds as float
 
 
 func _process(delta):
-	progress_bar.value = (power_system.current_power / power_system.target_power) * 100.0
-	
+	hud.power = (power_system.current_power / power_system.target_power) * 100.0
+
 	if game_running:
 		time_left -= delta
 		if time_left < 0:
