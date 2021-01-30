1e55092b6809514dd5d60c3ed4b3ec6d9b34bb72
SeleckyErik <35656626+SeleckyErik@users.noreply.github.com>
2021-01-30 01:24:30 +0100

    Make Human KinematicBody2D

--- Scripts/Human.gd
+++ Scripts/Human.gd
@@ -1,8 +1,11 @@
-extends RigidBody2D
+extends KinematicBody2D
 
 signal clicked
 
-var held = false
+const GRAVITY: = 75
+
+var velocity: = Vector2.ZERO
+var held: = false
 
 func _ready():
 	input_pickable = true
@@ -16,14 +19,18 @@ func _input_event(viewport, event, shape_idx):
 func _physics_process(delta):
 	if held:
 		global_transform.origin = get_global_mouse_position()
+		return
+
+	velocity.y += GRAVITY * delta
+	if move_and_collide(velocity):
+		velocity = Vector2.ZERO
+
 
 func pickup():
 	if held:
 		return
-	mode = RigidBody2D.MODE_STATIC
 	held = true
 
 func drop():
 	if held:
-		mode = RigidBody2D.MODE_RIGID
 		held = false
